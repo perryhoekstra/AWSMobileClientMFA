53bebb7ee444cc38268ab834e0fcc22564b21130
Perry Hoekstra <perry.hoekstra@gmail.com>
2019-11-08 10:07:12 -0600

    Begin to load new code.

---
+++ AWSMobileClientMFA/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  AWSMobileClientMFA
+//
+//  Created by Perry Hoekstra on 11/8/19.
+//  Copyright © 2019 Perry Hoekstra. All rights reserved.
+//
+
+import Foundation
