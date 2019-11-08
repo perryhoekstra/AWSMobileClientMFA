f99306738b16fc40e67db64186005ef12f3bded7
Perry Hoekstra <perry.hoekstra@gmail.com>
2019-11-08 06:43:02 -0600

    Initial Commit

---
+++ AWSMobileClientMFA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWSMobileClientMFA
+//
+//  Created by Perry Hoekstra on 11/8/19.
+//  Copyright © 2019 Perry Hoekstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
